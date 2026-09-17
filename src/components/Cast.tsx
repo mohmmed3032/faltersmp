06721c07b8e57121eec4dcf475acc124d09584fe
ItzRedFox3 <mohmmed2001.alghol2001.2@gmail.com>
2026-09-17 07:04:11 +0200

    dont worrie about what did i change

--- src/components/Cast.tsx
+++ src/components/Cast.tsx
@@ -44,7 +44,7 @@ const cast: CastMember[] = [
       "Part of the crew. A familiar face in the world of Falter SMP.",
     image: "/cast/rednax.png",
     youtube: "https://www.youtube.com/@Rednax600",
-    imageFit: "contain",
+    imageFit: "cover",
     imagePosition: "center",
     accentColor: "var(--blaze)",
   },
